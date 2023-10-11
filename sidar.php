dd431552cc8538d3f30a749665a1be15d7a35398
Dinakeri <117737175+Dinakeri@users.noreply.github.com>
2023-10-11 10:02:00 +0200

    Mij pagina af/Ik heb img folder toegevoegd

---
+++ sidar.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Sidar's pagina</title>
+</head>
+<body>
+    <h1>Ik wil naar huis.</h1>
+    <img src="img/Sidar.jpg" alt="Huis">
+</body>
+</html>
